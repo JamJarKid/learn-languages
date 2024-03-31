616934b7bc58224cf179a5262cb9490862d7ff3d
malijah <41331314+Malijah-C@users.noreply.github.com>
2024-04-01 00:25:53 +0100

    finished exercise 07

--- web/odin-project/foundations/javascript-exercises/07_tempConversion/tempConversion.js
+++ web/odin-project/foundations/javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  let tempC = ((temperature - 32) * 5) / 9;
+  return parseFloat(tempC.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+  let tempF = (temperature * 9) / 5 + 32;
+  return parseFloat(tempF.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
