8cb78cde6a8833ef45dfa57b1d5464a336604663
malijah <41331314+Malijah-C@users.noreply.github.com>
2024-03-31 23:49:43 +0100

    finished exercise 05

--- web/odin-project/foundations/javascript-exercises/05_sumAll/sumAll.js
+++ web/odin-project/foundations/javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (start, stop) {
+  let sum = 0;
 
+  if (start < 0 || stop < 0) {
+    return "ERROR";
+  } else if (typeof start !== "number" || typeof stop !== "number") {
+    return "ERROR";
+  }
+  if (start < stop) {
+    loopStart = start;
+    loopStop = stop;
+  } else {
+    loopStart = stop;
+    loopStop = start;
+  }
+  for (let i = loopStart; i <= loopStop; ++i) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
