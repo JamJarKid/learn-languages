2a9a4ba9908cbd0938d39a24d2b16ff5861a3434
malijah <41331314+Malijah-C@users.noreply.github.com>
2024-03-31 23:11:03 +0100

    finished exercise 04

--- web/odin-project/foundations/javascript-exercises/04_removeFromArray/removeFromArray.js
+++ web/odin-project/foundations/javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...nums) {
+  for (let num of nums) {
+    while (array.indexOf(num) > -1) {
+      array.splice(array.indexOf(num), 1);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
