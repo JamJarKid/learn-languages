a8ccfd5dd60daeb09eff59d0c6b6186d423fd626
malijah <41331314+Malijah-C@users.noreply.github.com>
2024-10-12 17:05:53 +0100

    completed 09-palindromes

--- web/odin-project/foundations/javascript-exercises/09_palindromes/palindromes.js
+++ web/odin-project/foundations/javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.toLowerCase()
+    const regex = /[A-Za-z]/g;
+    const wordArray = word.match(regex)
+    return wordArray.reverse() === wordArray
+    
 };
 
 // Do not edit below this line
